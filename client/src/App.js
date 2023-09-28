dbad3c5cf2e7bb1a9792c12b7921c3088e272bd4
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2023-09-28 12:04:38 -0400

    Added types to project.

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import Login from "./auth/Login";
-
-function App() {
-  return (
-    <Login />
-  );
-}
-
-export default App;
---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import Login from './auth/Login.jsx';
+
+function App() {
+	return <Login />;
+}
+
+export default App;
